d84ce5237c2d8eb9b03e7916f1aaa921314a564c
Diatemba99 <martin.diatta99@gmail.com>
2021-03-18 02:41:16 +0000

    learning how to print

---
+++ debut/src/debut22/Debut.java
@@ -0,0 +1,13 @@
+package debut22;
+
+public class Debut {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Debut!");
+		System.out.println("Je suis de retour  \"sama gayi\"");
+		
+
+	}
+
+}
